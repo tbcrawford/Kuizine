c0bbcc6af179fda6a6238e724af5c459aeb84205
Tyler Crawford <dev.tcrawford@gmail.com>
2016-02-05 16:27:20 -0500

    Initial upload - version 0.1

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+      
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('browseCtrl', function($scope) {
+
+})
+   
+.controller('favoritesCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('searchCtrl', function($scope) {
+
+})
+ 
